5ffd604c7d8ec2aed58d003fbced791b14a4fdab
Abanoub George <abanoub.g@trufla.com>
2021-11-08 17:58:15 +0200

    add config deploy

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export const Navbar = () => {
     return ( 
         <nav className="navbar">
-           <h1> The Blog Post</h1>
+           <h1> The Cookies Post</h1>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/create" style={{ backgroundColor:"#f1356d"}}>New Blog</Link>
